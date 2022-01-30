cf7424f77e92c18a20a2775015caa58580a7533e
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-01-30 12:32:06 +0530

    layout and home page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
+import styles from './Layout.module.scss'
 
 const Layout: FC = ({ children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <Navbar />
       {children}
       <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
+import { Button } from "../components/UI";
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -7,6 +10,13 @@ const Home: NextPage = () => {
       <Head>
         <title>EduPortal-CET</title>
       </Head>
+      <div className={styles.hero}>
+        <div>
+          <h1>GROW | BUILD | LEARN</h1>
+          <Button as="a" href="/learn">GET STARTED</Button>
+        </div>
+        <Image src={'/study.svg'} height={400} width={400} layout="fixed" />
+      </div>
     </div>
   );
 };
