b31a7abedf653af425cdc662be42ec37f03a845c
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-02-12 23:19:43 +0530

    defining schema

--- paris/src/model/forum.model.ts
+++ paris/src/model/forum.model.ts
@@ -1,13 +1,36 @@
-import mongoose from "mongoose";
-import { UserDocument } from "./user.model";
+import mongoose from 'mongoose';
+import { UserDocument } from './user.model';
+
+export interface CommentDocument extends mongoose.Document {
+  id: string;
+  user: UserDocument['_id'];
+  username: string;
+  content: string;
+};
 
 export interface PostDocument extends mongoose.Document {
-  user: UserDocument["_id"];
+  user: UserDocument['_id'];
   title: string;
   body: string;
+  likes: string[];
+  comments: [CommentDocument];
   createdAt: Date;
   updatedAt: Date;
 }
+
+const CommentSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const PostSchema = new mongoose.Schema(
   {
@@ -17,13 +40,15 @@ const PostSchema = new mongoose.Schema(
       unique: true,
       default: () => new mongoose.Types.ObjectId(),
     },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    title: { type: String, default: true },
-    body: { type: String, default: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    likes: { type: [String], default: [], required: false },
+    comments: { type: [CommentSchema], default: [], required: false }
   },
   { timestamps: true }
 );
 
-const Post = mongoose.model<PostDocument>("Forum", PostSchema);
+const Post = mongoose.model<PostDocument>('Forum', PostSchema);
 
 export default Post;
