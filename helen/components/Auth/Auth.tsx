a791be0a7c081b20da5c0b92c8a4d5ad38d5a357
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-02-19 20:28:04 +0530

    auth ui

--- helen/components/Auth/Auth.tsx
+++ helen/components/Auth/Auth.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import Link from "next/link";
+import React, { FC } from "react";
+import subjects from "../../lib/subjects";
+import years from "../../lib/years";
+import styles from './Auth.module.scss'
 
-const Auth = () => {
-  return <div></div>;
+interface AuthProps {
+  isRegister?: boolean
+}
+
+const Auth: FC<AuthProps> = ({ isRegister }) => {
+  if (isRegister) {
+    return (
+      <form className={styles.formStyles}>
+        <h2>Register | EduPortal-CET</h2>
+        <div className={styles.innerForm}>
+          <input type="text" placeholder="name" className={styles.inputBox} />
+          <input type="email" placeholder="email" className={styles.inputBox} />
+          <input type="password" placeholder="password" className={styles.inputBox} />
+          <input type="password" placeholder="confirm password" className={styles.inputBox} />
+          <input type="number" placeholder="registration number" className={styles.inputBox} />
+          <select name="year" className={styles.selectBox}>
+            <option value="none" selected disabled hidden>Year</option>
+            {years.map(y => <option key={y} value={y}>{y}</option>)}
+          </select>
+          <select name="branch" className={styles.selectBox}>
+            <option value="none" selected disabled hidden>Branch</option>
+            {subjects.map(sub => <option key={sub.id} value={sub.title}>{sub.title}</option>)}
+          </select>
+          <p>Already have an account? <Link href="/login">Login</Link></p>
+          <button type="submit" className={styles.submitBtn}>Register</button>
+        </div>
+      </form>
+    );
+  } else {
+    return (
+      <form className={styles.formStyles}>
+        <h2>Login | EduPortal-CET</h2>
+        <div className={styles.innerForm}>
+        <input type="email" placeholder="email" className={styles.inputBox} />
+          <input type="password" placeholder="password" className={styles.inputBox} />
+          <p>Don&apos;t have an account yet? <Link href="/register">Register</Link></p>
+          <button type="submit" className={styles.submitBtn}>Login</button>
+        </div>
+      </form>
+    );
+  }
 };
 
 export default Auth;
---
+++ helen/pages/register.tsx
@@ -0,0 +1,11 @@
+import Auth from "../components/Auth"
+
+const Register = () => {
+  return (
+    <>
+      <Auth isRegister />
+    </>
+  )
+}
+
+export default Register
