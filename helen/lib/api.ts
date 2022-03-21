ebc31155063525aa72c085aba545831d88a19038
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-03-22 00:19:28 +0530

    separation of logic of auth

--- helen/lib/api.ts
+++ helen/lib/api.ts
@@ -1,19 +1,44 @@
-import axios from 'axios';
+import instance from 'axios';
+import Cookies from 'js-cookie';
 
-async function get() {
-  const res = await axios.post('http://localhost:8000/api/sessions', {
-    email: 'sid@sid.com',
-    password: 'sidharth',
+const axios = instance.create({
+  baseURL: 'http://localhost:8000',
+});
+
+export async function login(email: string, password: string) {
+  const res = await axios.post('/api/sessions', {
+    email,
+    password,
   });
   console.log(res);
   const {
     data: { accessToken, refreshToken },
   } = res;
+  if (accessToken && refreshToken) {
+    Cookies.set('eh_token', accessToken);
+    Cookies.set('eh_refresh', refreshToken);
+  }
   axios.defaults.headers.common['x-refresh'] = refreshToken;
   axios.defaults.headers.common['authorization'] = accessToken;
 }
 
-async function getAccess() {
-  const res = await axios.get('http://localhost:8000/api/sessions');
+export async function getSessions() {
+  const res = await axios.get('/api/sessions');
   console.log(res);
 }
+
+export async function register() {}
+
+export async function logout() {}
+
+export async function getPosts() {}
+
+export async function getPost() {}
+
+export async function deletePost() {}
+
+export async function updatePost() {}
+
+export async function likePost() {}
+
+export async function createPost() {}
--- helen/pages/login.tsx
+++ helen/pages/login.tsx
@@ -1,10 +1,59 @@
-import Auth from '../components/Auth';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import Link from 'next/link';
+import styles from '../styles/Auth.module.scss';
 
 const Login = () => {
   return (
-    <>
-      <Auth />
-    </>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors: any = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={async (values, { setSubmitting }) => {
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form className={styles.formStyles}>
+          <h2>Register | EduPortal-CET</h2>
+          <div className={styles.innerForm}>
+            <Field
+              type="email"
+              name="email"
+              placeholder="email"
+              className={styles.inputBox}
+            />
+            <ErrorMessage name="email" component="div" />
+            <Field
+              type="password"
+              placeholder="password"
+              className={styles.inputBox}
+              name="password"
+            />
+            <ErrorMessage name="password" component="div" />
+            <p>
+              Don&apos;t have an account yet?{' '}
+              <Link href="/register">Register</Link>
+            </p>
+            <button
+              type="submit"
+              className={styles.submitBtn}
+              disabled={isSubmitting}
+            >
+              Login
+            </button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
