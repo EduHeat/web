65c6cfd0b7588e9032084852d4d13dd7dce74a31
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-03-26 21:36:43 +0530

    using example as fallback

--- helen/consts/college.ts
+++ helen/consts/college.ts
@@ -3,11 +3,11 @@ let data: any;
 try {
   data = require('../app.json');
 } catch (err) {
-  data = {};
+  data = require('../app[example].json');
 }
 
-export const LOGO = data.name ?? '';
+export const LOGO = data.name;
 
-export const SECRET_CODE = data.secretCode ?? '';
+export const SECRET_CODE = data.secretCode;
 
-export const SLOGAN = data.slogan ?? '';
+export const SLOGAN = data.slogan;
